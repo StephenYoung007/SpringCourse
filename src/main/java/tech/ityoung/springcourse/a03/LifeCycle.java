7f6abc1b5acabdaebcb1f0553abf52bd9220add0
Stephen <wy1309374767@gmail.com>
2022-05-24 00:10:50 +0800

    applicationcontext     MyBeanPostProcessor

--- src/main/java/tech/ityoung/springcourse/a03/LifeCycle.java
+++ src/main/java/tech/ityoung/springcourse/a03/LifeCycle.java
@@ -1,9 +1,8 @@
 package tech.ityoung.springcourse.a03;
 
-import lombok.Value;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -13,21 +12,32 @@ import javax.annotation.PreDestroy;
 @Slf4j
 public class LifeCycle {
 
+    /**
+     * run first
+     */
     public LifeCycle() {
         log.info("construction....................................");
     }
 
-//    @Autowired
-//    public void autowire(@Value("${JAVA_HOME}") String javaHome) {
-//        log.debug("dependency injection : {}", javaHome);
-//    }
+    /**
+     * run for the second
+     * @param javaHome
+     */
+    @Autowired
+    public void autowire(@Value("${JAVA_HOME}") String javaHome) {
+        log.info("dependency injection : {}.............................", javaHome);
+    }
 
+    /**
+     * run for third
+     */
     @PostConstruct
     public void init() {
         log.info("init........................................");
     }
 
     // 单例多礼不同时机
+    // run for the last
     @PreDestroy
     public void destroy() {
         log.info("destroy........................................");
--- src/main/java/tech/ityoung/springcourse/a03/MyBeanPostProcessor.java
+++ src/main/java/tech/ityoung/springcourse/a03/MyBeanPostProcessor.java
@@ -1,51 +1,61 @@
 package tech.ityoung.springcourse.a03;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.DestructionAwareBeanPostProcessor;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.stereotype.Component;
 
 import java.beans.PropertyDescriptor;
 
+@Component
+@Slf4j
 public class MyBeanPostProcessor implements InstantiationAwareBeanPostProcessor, DestructionAwareBeanPostProcessor {
-
     @Override
     public void postProcessBeforeDestruction(Object bean, String beanName) throws BeansException {
-
-    }
-
-    @Override
-    public boolean requiresDestruction(Object bean) {
-        return DestructionAwareBeanPostProcessor.super.requiresDestruction(bean);
+        if (beanName.equals("lifeCycle")) {
+            log.info("<<<<<<销毁之前执行，如@PreDestroy");
+        }
     }
 
     @Override
     public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+        if (beanName.equals("lifeCycle")) {
+            log.info("<<<<<<实例化之后执行，这里如果返回false会跳过依赖注入阶段");
+        }
         return InstantiationAwareBeanPostProcessor.super.postProcessBeforeInstantiation(beanClass, beanName);
     }
 
     @Override
     public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+        if (beanName.equals("lifeCycle")) {
+            log.info("<<<<<<实例化之后执行，这里如果返回false会跳过依赖注入阶段");
+        }
         return InstantiationAwareBeanPostProcessor.super.postProcessAfterInstantiation(bean, beanName);
     }
 
     @Override
     public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
+        if (beanName.equals("lifeCycle")) {
+            log.info("<<<<<依赖注入阶段执行，如@Autowired、@Value、@Resource");
+        }
         return InstantiationAwareBeanPostProcessor.super.postProcessProperties(pvs, bean, beanName);
     }
 
     @Override
-    public PropertyValues postProcessPropertyValues(PropertyValues pvs, PropertyDescriptor[] pds, Object bean, String beanName) throws BeansException {
-        return InstantiationAwareBeanPostProcessor.super.postProcessPropertyValues(pvs, pds, bean, beanName);
-    }
-
-    @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (beanName.equals("lifeCycle")) {
+            log.info("<<<<<<初始化之前执行，这里返回的对象会替换掉原本的bean,如@PostCons初始化");
+        }
         return InstantiationAwareBeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (beanName.equals("lifeCycle")) {
+            log.info("<<<<<<初始化之后执行，这里返回的对象会替换掉原本的bean,如代理增强");
+        }
         return InstantiationAwareBeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
     }
 }
